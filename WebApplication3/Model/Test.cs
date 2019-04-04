5eda5f30e824e2159b5ae1f1c7654386a67e83ff
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-04-04 09:10:57 +0300

    added model required field for model valid state

--- WebApplication3/Model/Test.cs
+++ WebApplication3/Model/Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace WebApplication3.Model
 
         public int Id { get; set; }
         public string author { get; set; }
+
+        [Required]
         public string name { get; set; }
         public string description { get; set; }
 
