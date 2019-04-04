03f24a3b81c265f2340be0e102c0c3b4833ef2bc
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-04-04 06:51:25 +0300

    ConfigureServices is updated with dbConnection

---
+++ WebApplication3/Model/DbModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3.Model
+{
+    public class DbModel : DbContext
+    {
+        public DbModel(DbContextOptions<DbModel> options) : base(options) {
+        }
+    }
+}
